795dd637f7bc40458405040ba321fd4937ddceb4
liam <paledeepened@gmail.com>
2024-04-08 13:20:28 +0100

    Implements operation button functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//TODO
-//Make display font smaller the less space there is
+//TODO Make display font smaller the less space there is
 
 function updateDisplay(currentNumber){
     const display = document.getElementById("display-text");
@@ -10,17 +9,46 @@ function convertToNumber (array){
     return Number(array.join(''))
 }
 
-function subtract (previousNumber, currentNumber){
+function getOperationAnswer (previousNumber, currentNumber, operation){
     previousNumber = convertToNumber(previousNumber);
     currentNumber = convertToNumber(currentNumber);
-    return previousNumber - currentNumber;
+    let answer = 0;
+
+    switch(operation){
+        case "+":
+            answer = previousNumber + currentNumber;
+            break;
+        case "-":
+            answer = previousNumber - currentNumber;
+            break;
+        case "*":
+            answer = previousNumber * currentNumber;
+            break;
+        case "/":
+            currentNumber === 0 ? answer = 8008135 : answer = previousNumber / currentNumber;
+            break;
+            }
+        
+    return String(answer).includes(".") ? answer.toFixed(7) : answer;
 }
 
-const buttons = Array.from(document.querySelectorAll("button"));
+function performOperation(button){
+    operation = button.textContent;
+    if (previousNumber.length < 1){
+        previousNumber = currentNumber;
+        currentNumber = [];
+        updateDisplay(currentNumber);
+    }
+    else{
+        answer = getOperationAnswer(previousNumber, currentNumber, operation);
+        previousNumber = String(answer).split('');
+        updateDisplay(previousNumber);
+        currentNumber = [];
+    }
+}
 
 let previousNumber = []
 let currentNumber = [] 
-let operation = ''
 
 const numberButtons = document.querySelectorAll(".button-number");
 numberButtons.forEach(button => button.addEventListener("click", () => {
@@ -44,31 +72,12 @@ decimalButton.addEventListener("click", () => {
     }
 })
 
-const subtractButton = document.getElementById("button-subtraction");
-subtractButton.addEventListener("click", () => {
-    if (currentNumber.length < 1){
-        currentNumber.push("-");
-        updateDisplay(currentNumber)
-    } 
-    else{
-        if (previousNumber.length < 1){
-            previousNumber = currentNumber;
-            currentNumber = [];
-            updateDisplay(currentNumber)
-        }
-        else{
-            answer = subtract(previousNumber, currentNumber); 
-            previousNumber = String(answer).split('');
-            updateDisplay(previousNumber);
-            currentNumber = []; 
-        }
-    }
-})
-
-
-
-const operatorButtons = document.querySelectorAll(".button-operator");
-
+const operationButtons = document.querySelectorAll(".button-operator");
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        performOperation(button, previousNumber, currentNumber);
+    });
+});
 
 const clearButton = document.getElementById("button-clear");
 clearButton.addEventListener("click", () => {
@@ -77,23 +86,3 @@ clearButton.addEventListener("click", () => {
     operation = ''
     updateDisplay(currentNumber);
 })
-
-//previous number is 0
-//current number and previous number is operated on?
-
-//Handle clear that will empty calcInput and clear both displays
-
-//Handle back button by having it remove last index from calcInput
-
-//Handle equals by having it evaluate the array of calc input and displaying result
-
-//Need only one decimal
-// if decimal is inside calcInput don't do anything else add the decimal
-
-//handle divide by 0
-
-//handle negative numbers
-
-//number buttons add to currentNumber
-//if decimal in currentNumber decimal button does nothing which is how it works on apple calculator
-
